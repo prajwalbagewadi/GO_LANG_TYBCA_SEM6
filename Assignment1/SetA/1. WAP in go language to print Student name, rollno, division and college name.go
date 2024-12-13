34623ee08ab4bd9124071a37a37af8ee4eac7b59
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2024-12-13 09:58:17 +0530

    Create 1. WAP in go language to print Student name, rollno, division and college name.go

---
+++ Assignment1/SetA/1. WAP in go language to print Student name, rollno, division and college name.go
@@ -0,0 +1,18 @@
+/*
+1. WAP in go language to print Student name, rollno, division and college name
+*/
+
+package main
+
+import ("fmt")
+
+func main(){
+    var studname string = "Prajwal"
+    var rollno int = 04
+    var div string ="A"
+    var clgname string ="Dr DY PATIL ACS CLG"
+    fmt.Printf("Student name=%s\n",studname)
+    fmt.Printf("Student rollno=%d\n",rollno) 
+    fmt.Printf("Student div=%s\n",div) 
+    fmt.Printf("Student clgname=%s\n",clgname)
+}
